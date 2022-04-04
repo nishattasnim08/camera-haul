b9fefa1744411e6729338e15fb75975b4f2cf464
nishattasnim08 <nishattasnim24258@gmail.com>
2022-04-04 23:21:42 +0600

    navbar commited

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <nav className='header'>
+            <a href="">Home</a>
+            <a href="">Review</a>
+            <a href="">Dashboard</a>
+            <a href="">Blogs</a>
+            <a href="">About</a>
+        </nav>
+    );
+};
+
+export default Header;
